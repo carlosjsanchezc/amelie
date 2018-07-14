493e1c1fd1cd055429d240cd8ef3c16d074004ad
CARLITOS\Carlos Sánchez <carlosjsanchezc@gmail.com>
2018-07-14 03:02:39 -0400

    14072018

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -9,10 +9,18 @@ import { HttpconnectProvider } from '../../providers/httpconnect/httpconnect';
 export class ContactPage {
   id_usuario:number;
   id_exchanger:number;
+  balancerobot:number;
+  balance:number;
   ordenesa:any[]=[];
   ordenesc:any[]=[];
+  ordenesr:any[]=[];
+  ordenesnr:any[]=[];
+  
+  monedas:any[]=[];
+  monedas_server:any[]=[];
   constructor(public navCtrl: NavController,public httpService:HttpconnectProvider) {
 this.cargahistorial();
+this.cargarmonedas();
   }
 
 
@@ -41,14 +49,91 @@ this.cargahistorial();
     });
   }
 
+  cargarmonedas()
+  {
+    let id=this.httpService.id_usuario;
+    console.log(id);
+    let url="https://lycexpress.com/amelie/monedas.php?id_usuario="+id;
+    
+    this.httpService.httpr(url).subscribe((data) => 
+    
+    {
+      console.log('Data monedas');
+      console.log(data);
+      if (data['success']=='true')
+      {
+        this.monedas=data['coins'];
+        console.log(this.monedas.length);
+        for (let index = 0; index < this.monedas.length; index++) {
+          this.monedas_server.push(false);
+          
+        }
+        
+        console.log(this.monedas);
+      }
+
+    });
+
+  }
+
   cargahistorial()
   {
     let url="https://lycexpress.com/amelie/apiamelie.php?id_usuario="+this.httpService.id_usuario+"&opcion=5";
     this.httpService.httpr(url).subscribe((data) => 
     {
       this.ordenesa=data['abiertas'];
-      console.log('Longitud:');
-      console.log(this.ordenesa);
+      this.ordenesr=data['resumen'];
+      this.ordenesnr=data['resumennr'];
+      console.log("Ordenes manuales");
+      console.log(this.ordenesnr);
+      this.balance=data['balance'];
+      this.balancerobot=data['balancerobot'];
+      
+      console.log('cerradas:');
+      console.log(this.ordenesc);
+      console.log('Resumen:xxxx');
+      console.log(this.ordenesr);
+      console.log('Balance');
+      console.log(data['balance']);
+      console.log("monedas");
+      console.log(this.monedas);
+      for (let index = 0; index < this.ordenesr.length; index++) {
+        for (let k = 0; k < this.monedas.length; k++) {
+          var s3:string;
+          s3=this.ordenesr[index].moneda;
+
+          var s=s3.indexOf(this.monedas[k].simbolo);
+
+
+          if (s!=-1){
+
+            this.ordenesr[index].imagen=this.monedas[k].imagen;
+          }
+
+
+          
+        }
+      }
+
+
+      for (let index = 0; index < this.ordenesnr.length; index++) {
+        for (let k = 0; k < this.monedas.length; k++) {
+          var s3:string;
+          s3=this.ordenesnr[index].moneda;
+
+          var s=s3.indexOf(this.monedas[k].simbolo);
+
+
+          if (s!=-1){
+
+            this.ordenesnr[index].imagen=this.monedas[k].imagen;
+          }
+
+
+          
+        }
+      }
+
       for (let index = 0; index < this.ordenesa.length; index++) {
         
         if (this.ordenesa[index].side=="sell") 
@@ -94,6 +179,30 @@ this.cargahistorial();
         }
 
       }
+      for (let index = 0; index < this.ordenesc.length; index++) 
+      {
+
+        if (this.ordenesc[index]=="Venta")
+        {
+          for (let k = 0; index < this.ordenesc.length; k++) 
+          {
+            if (this.ordenesc[k].side=="Compra")
+            {
+              if ((this.ordenesc[index].amount==this.ordenesc[k].amount)||(this.ordenesc[index].price==this.ordenesc[k].price))
+              {
+              
+              }
+            }
+          }
+        }
+
+    
+    
+  
+        
+      }
+
+
       console.log('ordenes:');
       console.log(this.ordenesa);
     });
